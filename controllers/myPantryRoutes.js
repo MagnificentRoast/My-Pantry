f7919966b0582e03b674915b8b88fc5b34a32de0
Kiri Anthony <Kiriko.anthony@gmail.com>
2022-03-29 20:52:36 -0700

    adding myPantryRoute

--- controllers/myPantryRoutes.js
+++ controllers/myPantryRoutes.js
@@ -1,8 +1,47 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const {Food, Pantry, User} = require('../models');
 
+// get all foods for pantry page
 router.get('/', (req, res) => {
-    console.log("Endpoint is working");
-    res.render('my-pantry');
+    console.log(req.session);
+    Food.findAll({
+        // where: {
+//     pantry_id: req.session.pantry_id
+// },   
+        attributes: [
+            'id',
+            'food_name',
+            'pantry_id'
+        ],
+        include: [
+            {
+                model: Pantry,
+                attributes: ['id'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            // {
+            //     model: User,
+            //     attributes: ['username']
+            // }
+        ]
+    })
+    .then(dbFoodData => {
+        const foods = dbFoodData.map(food => food.get({plain: true}));
+        res.render('my-pantry', {foods, loggedIn: true});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+
 });
 
 module.exports = router;
+
+// where: {
+//     pantry_id: req.session.pantry_id
+// },
