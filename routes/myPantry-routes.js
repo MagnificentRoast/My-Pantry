bbafcda68a38cc9916d8b02bc2c9c1d725cc56c7
Kiri Anthony <Kiriko.anthony@gmail.com>
2022-03-29 17:25:37 -0700

    myPantry-routes update

--- routes/myPantry-routes.js
+++ routes/myPantry-routes.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
-// const sequelize = require('../config/connection');
-// const {Food, Pantry, User} = require('../models');
-
-console.log("hello");
+const sequelize = require('../config/connection');
+const {Food, Pantry, User} = require('../models');
 
 router.get('/', (req, res) => {
+    console.log(req.session);
+    Food.findAll({
+        where: {
+            user_id: req.session.user_id
+        }
+    })
     res.render('my-pantry');
 });
 
