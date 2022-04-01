7b363f01c3ca023e6ee1bb1e9971f7b4f87e903f
Jonathan W <jwilliams12117@gmail.com>
2022-03-31 19:39:32 -0700

    Fixed bug that skipped every other checkbox when deleting

--- public/javascript/pantry.js
+++ public/javascript/pantry.js
@@ -1,6 +1,3 @@
-// selectors
-const addFoodBtn = document.querySelector("#addFood");
-const deleteFoodBtn = document.querySelector("#deleteFood");
 
 // Array to hold foods that are to be deleted
 let checkedFoods = [];
@@ -9,7 +6,6 @@ let checkedFoods = [];
 
 // Create food button handler
 const addFoodBtnHandler = () => {
-    console.log("Adding a new food.");
 
     // Display a new text field that will use autocomplete
     const newFoodSearch = document.createElement("input");
@@ -64,22 +60,16 @@ const toggleFood = (checkboxElement) => {
 
     // If that ID is not in the array, add it.
     if (!checkedFoods.includes(foodID)) {
-        console.log("This ID is not in checkedFoods Array.");
         checkedFoods.push(foodID);
     }
     // Otherwise, remove it.
     else {
         checkedFoods = checkedFoods.filter(id => id !== foodID);
     }
-
-    console.log("Checked foods: "+checkedFoods);
-
 }
 
 // Delete food button: appears/highlights once any checkboxes are checked
 const deleteFoodBtnHandler = () => {
-
-    console.log("Deleting checked foods");
 
     // Organize all <li> elements into an array for sorting/deletion
     let pantryFoods = document.getElementsByClassName("food-li");
@@ -91,34 +81,15 @@ const deleteFoodBtnHandler = () => {
         // If the ID is there, delete the <li> element entirely
         if (checkedFoods.includes(foodID)) {
             pantryFoods[i].remove();
-        };
+            i--;
+        }
     }
     // Reset the value of checkedFoods to be an empty array
     checkedFoods = [];
-
-    // The below code should access the "checked" property of the checkboxes but for some reason doesn't,
-    // so now I had to introduce a toggleFood function
-    // for (let i = 0; i < pantryFoods.length; i++) {
-    //     let children = pantryFoods[0].children;
-    //     console.log(children[1].checked);
-    //     if (children[1].checked === true) {
-    //         console.log("The box is checked.");
-    //     }
-    //     else {
-    //         console.log("The box is unchecked.");
-    //     }
-    // }
-
-    // let checkedFoods = pantryFoods.map(food => {
-    //     food.value = "false";
-    //     console.log(food);
-    // });
-    // console.log(checkedFoods);
 }
 
 document.querySelector("#foods-list").addEventListener("click", event => {
     if (event.target.getAttribute("class") === "delete-checkbox") {
-        console.log("Clicked a checkbox");
         toggleFood(event.target);
     }
 })
