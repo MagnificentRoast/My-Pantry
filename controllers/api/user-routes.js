4cdeea63246f742a2b0d4c38eef5874ba8fe9222
MagnificentRoast <94555704+MagnificentRoast@users.noreply.github.com>
2022-03-31 19:48:56 -0700

    added login route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -102,7 +102,32 @@ router.post('/logout', (req, res) => {
 
 // POST request to log in
 router.post("/login", (req, res) => {
-    
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        req.session.save(() => {
+            // declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in!'});
+        });
+    })
 })
 
 // Modify existing user data
@@ -151,5 +176,6 @@ router.delete("/:id", (req, res) => {
         res.status(500).json(err);
     });
 })
+});
 
 module.exports = router;
