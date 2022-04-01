74e18c81e1b3182c881edec777b6a73023df1248
Jonathan W <jwilliams12117@gmail.com>
2022-03-31 22:32:58 -0700

    Cleaned up references in front-end javascript

--- public/javascript/homejava.js
+++ public/javascript/homejava.js
@@ -26,43 +26,28 @@ async function loginFormHandler(event) {
 
 // event handler function for sign-up
 async function signupFormHandler(event) {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (username && password) {
-      const response = await fetch('/api/users', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      })
-      .then(userData => {
-        console.log(userData);
-      })
-      // if (response.ok) {
-      //   // Fetch a POST request to create a new pantry
-      //   fetch("/api/pantry", {
-      //     method: "post",
-      //     body: JSON.stringify({
-      //       user_id: req.session.user_id
-      //     }),
-      //     headers: { 'Content-Type': 'application/json' }
-      //   })
-      //   .then(() => {
-      //     // redirect them to /my-pantry
-      //     document.location.replace('/my-pantry')
-      //   })
-      .catch((err) => {
-        response.status(500).json(err);
-      });
+	event.preventDefault();
 
-    } else {
-      alert(response.statusText);
-    }
+	const username = document.querySelector('#username-signup').value.trim();
+	const password = document.querySelector('#password-signup').value.trim();
+
+	if (username && password) {
+	const response = await fetch('/api/users', {
+		method: 'post',
+		body: JSON.stringify({
+		username,
+		password
+		}),
+		headers: { 'Content-Type': 'application/json' }
+	});
+
+	if (response.ok) {
+		// redirect them to /my-pantry
+		document.location.replace('/my-pantry')     
+	} else {
+		alert(response.statusText);
+		}
+	}
 }
 
 // event listener for login
